8a05da60c9cedde6ed13cd68e5f6ceb99ab4b4c5
Raphipod <raphipod@googlemail.com>
2021-01-09 01:01:31 +0100

    added "install-desktop.sh"

---
+++ setup/install-desktop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#----------------------------------------------------------------
+# This script installs the userspace (DE/WM) of GLinux.
+#
+# Copyright Raphipod (C) 2021 - Project GLinux
+#----------------------------------------------------------------
+
+# make sure everything is up-to-date
+
+sudo apt update
+
+echo "Everything is updated. Proceeding with installing the DE."
+
+# install Desktop Environment
